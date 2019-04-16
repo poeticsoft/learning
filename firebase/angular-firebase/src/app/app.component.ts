fbc608d0779b1c296a96a3adc24b2ca5b4080f32
albertomoral <alberto.moral@poeticsoft.com>
2019-04-16 20:58:37 +0200

    Firebase auth

--- firebase/angular-firebase/src/app/app.component.ts
+++ firebase/angular-firebase/src/app/app.component.ts
@@ -1,21 +1,91 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 
-import { AngularFirestore } from '@angular/fire/firestore';
+import { Component } from '@angular/core';
+import { 
+  Observable,
+  Subscription,
+  interval
+} from 'rxjs';
+import { 
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+  DocumentChangeAction
+} from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 // https://github.com/angular/angularfire2/blob/master/docs/install-and-setup.md
+// https://firebase.google.com/docs/auth/web/manage-users?hl=es-419
+
+interface Product { 
+  ProductName: string; 
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  products: Observable<any[]>;
+  public productsCollection: AngularFirestoreCollection<Product>;
+  public productsSubscription: Subscription;
+  public products: Product[] = [];
+  public UserLoged: boolean = false;
 
-  constructor(db: AngularFirestore) {
+  constructor(
+    public afs: AngularFirestore,
+    public afa: AngularFireAuth
+  ) {
 
-    this.products = db.collection('products').valueChanges();
+    this.afa.auth.onAuthStateChanged(this.userStateChanged.bind(this));
+    this.productsCollection = afs.collection('products');
+  }
+
+  setProducts(Products) {
+
+    this.products = Products;
+  }
+
+  addProduct() {
+
+    this.productsCollection.add({
+      ProductName: 'Name ?'
+    });
+  }
+
+  userStateChanged(user) {
+
+    if (user) {
+
+      this.UserLoged = true;
+
+      this.productsSubscription = this.productsCollection.valueChanges()
+      .subscribe(this.setProducts.bind(this));
+
+    } else {
+
+      this.UserLoged = false;
+
+      if(this.productsSubscription)
+        this.productsSubscription.unsubscribe();
+    }
+  }
+
+  login() {
+
+    this.afa.auth.signInWithEmailAndPassword(
+      'alberto.moral@poeticsoft.com',
+      'JsAU8)0000'
+    ).catch(function(Error) {
+      
+      console.log(Error);
+    });
+  }
+
+  logout() {
+
+    this.afa.auth.signOut()
+    .then(() => { })
+    .catch(() => { });
   }
 }
--- firebase/angular-firebase/src/app/app.module.ts
+++ firebase/angular-firebase/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from  '@angular/fire/firestore';
-
+import { AngularFireAuthModule  } from '@angular/fire/auth';
 import { AppComponent } from './app.component';
-
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -15,7 +13,8 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase.config),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
